33cf58b7731b98681004ed91af9d729ead6e5d29
Godwin Iyogun <giyogun@gmail.com>
2022-04-12 15:37:56 +0100

    add content and styling to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      You're Home!
-    </div>
+    <main>
+      <img src="http://res.cloudinary.com/giyogun/image/upload/v1649773729/blog_images/mq7wf27hiv26h28is4bh.jpg" alt="header pic" className="absolute object-cover w-full h-full" />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading snug home-name">Yoohoo! I'm Godwin</h1>
+      </section>
+    </main>
   )
 }
 
